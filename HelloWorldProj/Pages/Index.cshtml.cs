9abc78f1fdef438bd7d55730dda10527326ba804
KAMRAN <kamranrizwan044@gmail.com>
2020-04-15 14:10:58 +0500

    Assignment 1 : Delegates and events + Signleton pattern

---
+++ HelloWorldProj/Pages/Index.cshtml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorldProj.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            Number myNumber = new Number(100000);
+            myNumber.PrintMoney();
+            myNumber.PrintNumber();
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+    public class Number
+    {
+        private PrintHelper _printHelper;
+
+        public Number(int val)
+        {
+            _value = val;
+
+            _printHelper = PrintHelper.Instance;
+            //subscribe to beforePrintEvent event
+            _printHelper.beforePrintEvent += printHelper_beforePrintEvent;
+        }
+        //beforePrintevent handler
+        void printHelper_beforePrintEvent(string message)
+        {
+            Console.WriteLine("BeforePrintEvent fires from {0}", message);
+        }
+
+        private int _value;
+
+        public void PrintMoney()
+        {
+            _printHelper.PrintMoney(_value);
+        }
+
+        public void PrintNumber()
+        {
+            _printHelper.PrintNumber(_value);
+        }
+
+    }
+    public sealed class PrintHelper
+    {
+        public delegate void BeforePrint(string message);
+        public event BeforePrint beforePrintEvent;
+
+        private PrintHelper()
+        {
+
+        }
+        private static PrintHelper instance = null;
+        public static PrintHelper Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new PrintHelper();
+                }
+                return instance;
+            }
+        }
+        public void PrintNumber(int num)
+        {
+            if (beforePrintEvent != null)
+                beforePrintEvent.Invoke("PrintNumber");
+
+            Console.WriteLine("Number: {0,-12:N0}", num);
+
+        }
+
+        public void PrintDecimal(int dec)
+        {
+            if (beforePrintEvent != null)
+                beforePrintEvent("PrintDecimal");
+
+            Console.WriteLine("Decimal: {0:G}", dec);
+        }
+
+        public void PrintMoney(int money)
+        {
+            if (beforePrintEvent != null)
+                beforePrintEvent("PrintMoney");
+
+            Console.WriteLine("Money: {0:C}", money);
+        }
+
+        public void PrintTemperature(int num)
+        {
+            if (beforePrintEvent != null)
+                beforePrintEvent("PrintTemerature");
+
+            Console.WriteLine("Temperature: {0,4:N1} F", num);
+        }
+        public void PrintHexadecimal(int dec)
+        {
+            if (beforePrintEvent != null)
+                beforePrintEvent("PrintHexadecimal");
+
+            Console.WriteLine("Hexadecimal: {0:X}", dec);
+        }
+    }
+}
